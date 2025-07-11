270d980bdcdc5eaab4a38b4c2013bf40c2b51bb6
Ilovko <seowooseokqkqh@naver.com>
2025-07-11 10:02:18 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'R6S static help',
+  description: 'for every r6s player',
   generator: 'v0.dev',
 }
 
